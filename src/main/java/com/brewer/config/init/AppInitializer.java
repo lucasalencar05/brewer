64e37402db3d2879e99195fcd77c9d17540bb349
Lucas Alencar <lucasbalencar05@gmail.com>
2020-06-26 18:25:38 -0300

    Commit config Spring and Thymeleaf

--- src/main/java/com/brewer/config/init/AppInitializer.java
+++ src/main/java/com/brewer/config/init/AppInitializer.java
@@ -1,7 +1,10 @@
 package com.brewer.config.init;
 
 import com.brewer.config.WebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -17,5 +20,14 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        return new Filter[] { characterEncodingFilter };
     }
 }
--- src/main/java/com/brewer/controller/CervejasController.java
+++ src/main/java/com/brewer/controller/CervejasController.java
@@ -1,13 +1,31 @@
 package com.brewer.controller;
 
+import com.brewer.model.Cerveja;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class CervejasController {
 
     @RequestMapping("/cervejas/novo")
-    public String novo() {
+    public String create(Cerveja cerveja) {
         return "cerveja/create";
     }
+
+    @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+    public String store(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+
+        if(result.hasErrors()) {
+            return create(cerveja);
+        }
+        attributes.addFlashAttribute("message", "Sucesso, cadastro realizado");
+        return "redirect:/cervejas/novo";
+    }
+
 }
